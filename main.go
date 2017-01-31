8660e1703352a5cbf219d208dc3c652146942f9e
Steven <steven@rootedlogics.com>
2017-01-31 14:34:51 -0500

    listening and serving routes based on routes.json, basic 404 handling

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 )
 
-
 /*
  * Parsing of JSON
  */
@@ -22,26 +21,49 @@ type Request struct {
 }
 
 type Response struct {
+	Code int
 	Body string `json:"body"`
 }
-
 
 /*
  * Slice of Route structs
  */
 type Routes []Route
 
-
 /*
  * Setting up HTTP server
  */
 type ServerConfig struct {
 	Address string
-	Port	int
+	Port    int
 }
 
 type Server struct {
 	Config Config
+}
+
+func (server Server) getResponse(request *http.Request) Response {
+	//getting our routes from JSON
+	for _, route := range server.Config.Routes {
+		//if Path and Method Match
+		if route.Request.Path == request.URL.Path && route.Request.Method == request.Method {
+			//Respond with route response JSON
+			return route.Response
+		}
+	}
+	//else return bad response
+	return Response{Code: 404, Body: "Not Found"}
+}
+
+func (server Server) Handle(writer http.ResponseWriter, request *http.Request) {
+	response := server.getResponse(request)
+	fmt.Fprintf(writer, response.Body)
+}
+
+func (server Server) Run() {
+	http.HandleFunc("/", server.Handle)
+	addr := fmt.Sprintf("%v:%v", server.Config.Server.Address, server.Config.Server.Port)
+	panic(http.ListenAndServe(addr, nil))
 }
 
 /*
@@ -52,10 +74,6 @@ type Config struct {
 	Routes Routes `json:"routes"`
 }
 
-func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "hello World, %s!", request.URL.Path[1:])
-}
-
 func main() {
 	apiRoutes, err := ioutil.ReadFile("routes.json")
 	if err != nil {
@@ -64,15 +82,17 @@ func main() {
 
 	fmt.Println(string(apiRoutes))
 
-	serverConfig := ServerConfig{ Address: "0.0.0.0", Port: 8080 }
+	serverConfig := ServerConfig{Address: "0.0.0.0", Port: 8080 }
 
 	var config Config
 	json.Unmarshal(apiRoutes, &config)
+
 	config.Server = serverConfig
 	fmt.Println(config)
 
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
-
+	//http.HandleFunc("/", Handler)
+	//http.ListenAndServe(":8080", nil)
+	server := Server{config}
+	server.Run()
 
 }
